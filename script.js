ef03639f45d57d151d2843711a79244db677aefb
chiqq <nueh2549@gmail.com>
2026-08-22 12:28:11 +0700

    add .filter .map

---
+++ script.js
@@ -0,0 +1,35 @@
+let users = [
+    { id: 1, name: "สมชาย", ageGroup: "18-25", score: 85 },
+    { id: 2, name: "สมศรี", ageGroup: "ต่ำกว่า 18", score: 40 },
+    { id: 3, name: "สมปอง", ageGroup: "26 ขึ้นไป", score: 60 }
+];
+
+// let ppScore = users.checkScore();
+// function checkScore() {
+//     Object.fromEntries(Object.entries(data).filter(([key, value]) => value > 50))
+// }
+
+// let ppScore = Object.fromEntries(Object.entries(users).filter(([key, value]) => value > 50))
+// console.log(ppScore);
+
+const filteredUsers = users.filter((users) => {
+    return users.score > 50;
+})
+console.log(filteredUsers)
+
+const updatedUsers = users.map((users) => {
+    return {
+        name : users.name,
+        ageGroup : users.ageGroup,
+        score : users.score + 50
+    }        
+})
+
+console.log(updatedUsers)
+
+function randomDiscount() {
+    const randomNum = Math.floor(Math.random()*50) + 10;
+    console.log(randomNum);
+}
+
+randomDiscount();
